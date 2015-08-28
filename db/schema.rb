f87be2bc4d4a076c6ff23d7735eed21c0038cf5c
Mukul Gupta <mukulgupta.smu@gmail.com>
2015-08-28 11:52:51 -0300

    added the products scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150827165020) do
+ActiveRecord::Schema.define(version: 20150828144237) do
+
+  create_table "products", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "price"
+    t.integer  "points"
+    t.boolean  "availability", default: true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
