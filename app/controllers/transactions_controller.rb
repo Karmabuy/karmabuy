dec0be64e69be9a136a410d9c1fec92716a1ef83
Mukul Gupta <me@mukul.co>
2015-09-06 20:42:36 +0000

    Added state to sales

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,22 +2,18 @@ class TransactionsController < ApplicationController
     def create
     product = Product.find_by!(slug: params[:slug])
     token = params[:stripeToken]
-    
-    begin 
-    charge = Stripe::Charge.create(
+    sale = product.sales.create(
         amount: product.price,
-        currency: "cad",
-        card: token,
-        description: current_user.email
-        )
-        @sale = product.sales.create!(
-            buyer_email: current_user.email)
-            redirect_to pickup_url(guid: @sale.guid)
-            rescue Stripe::CardError => e
-            @error = e
-            redirect_to product_path(product), notice: @error
+        buyer_email: current_user.email,
+        seller_email: product.user.email,
+        stripe_token: params[:stripeToken])    
+        sale.process!
+        if sale.finished?
+            redirect_to pickup_url(guid: sale.guid), notice: "Transaction Successful"
+        else
+            redirect_to product_path(product), notice: "Something went wrong"
+        end
     end
-end
     def pickup
         @sale = Sale.find_by!(guid: params[:guid])
         @product = @sale.product
